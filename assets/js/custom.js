995179b966477bc08b0f377e8f1e6625b6d9731b
saadaf ahmed <hafijnahid45@gmail.com>
2021-01-19 03:44:40 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    new WOW().init();
+    $('body').materialScrollTop();
+    $('#our_popup').magnificPopup({ type: 'image' });
+
+    $('html').smoothScroll(1000);
+
+    $('.counter').counterUp({
+        delay: 50,
+        time: 5000
+    });
+    $('.manu').slicknav();
+
+    AOS.init({
+        duration: 2000,
+    });
+
+    // $('.header-slogan').ripples({
+    //     dropRadius: 10,
+    //     perturbance: 0.03,
+    // });
+
+    $('#main-menu').meanmenu({
+      meanMenuContainer:'#mobail-menu',
+
+      meanScreenWidth:768,
+    });
+
+  // var typed = new Typed('#typed', {
+  //    stringsElement: '#typed-strings'
+  //   });
+
+});
+
+// red rose
